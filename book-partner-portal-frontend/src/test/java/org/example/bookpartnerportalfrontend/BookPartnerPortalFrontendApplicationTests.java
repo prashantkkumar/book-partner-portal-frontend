58b8da31fb1cdafaec784eafe1be758a0d49233c
PrashantKumar_2110991043 <prashant1043.be21@chitkara.edu.in>
2025-06-11 12:44:01 +0530

    Commiting into dev

---
+++ book-partner-portal-frontend/src/test/java/org/example/bookpartnerportalfrontend/BookPartnerPortalFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bookpartnerportalfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookPartnerPortalFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
